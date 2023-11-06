7ac5aafbc280776e6189e937f8e423549acee7b0
sonpham28052002 <sonpham28052002@gmail.com>
2023-11-06 10:53:41 +0700

    create project

---
+++ App.js
@@ -0,0 +1,4 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import screen_1 from './screen/screen'
+export default screen_1
---
+++ screen/screen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View,Text,TextInput,Image} from 'react-native'
+
+export default function screen_1()
+{
+    return(
+        <View style={{flex:1, padding:10}}>
+            <Text>
+                Welcome, Jala
+            </Text>
+            <Text style={{}}>
+                Choice you Best food
+            </Text>
+        </View>
+    );
+}

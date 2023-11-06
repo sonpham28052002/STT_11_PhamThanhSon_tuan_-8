d9a83e123963fb6046c3b8a8afd70ac43e97cce1
sonpham28052002 <sonpham28052002@gmail.com>
2023-11-06 11:06:30 +0700

    update project

--- screen/screen.js
+++ screen/screen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View,Text,TextInput,Image} from 'react-native'
+import {View,Text,TextInput,Image,TouchableOpacity} from 'react-native'
 
 export default function screen_1()
 {
@@ -8,9 +8,17 @@ export default function screen_1()
             <Text>
                 Welcome, Jala
             </Text>
-            <Text style={{}}>
+            <Text style={{fontWeight:"bold", fontSize:20}}>
                 Choice you Best food
             </Text>
+            <TextInput style={{borderWidth:1, borderColor:"gray", marginTop:10, height:40, width:"80%"}}
+                placeholder='Search food'
+            />
+            <View style={{flexDirection:"row", justifyContent:"space-around"}}>
+                <TouchableOpacity style={{height:50, width:90, backgroundColor:"yellow", borderWidth:1}}>
+                    <Text>Donut</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
